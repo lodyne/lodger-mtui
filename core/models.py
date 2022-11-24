0a4b631980dc6e7f6a3d7b0569152148cf7ff30e
lodyne <lodgmtui@gmail.com>
2022-11-24 10:33:20 +0300

    create(model):design database in form of models
    ~ create models and make migrations
    ~ migrate all data into database
    ~ create super user
    ~ add urls.py into core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,51 @@
 from django.db import models
-
+from django.core.validators import RegexValidator
 # Create your models here.
+
+class Patient(models.Model):
+    firstname = models.CharField(max_length=50)
+    middlename = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    age = models.PositiveSmallIntegerField()
+    address = models.CharField(max_length=50)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$')
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    created_at = models.DateField(auto_now=False, auto_now_add=False)
+    updated_at = models.DateField(auto_now=False, auto_now_add=False)
+    
+
+    def __str__(self):
+        return (f"{self.firstname} {self.surname}")
+    
+
+class Doctor(models.Model):
+    firstname = models.CharField(max_length=50)
+    middlename = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    created_at = models.DateField(auto_now=False, auto_now_add=False)
+    updated_at = models.DateField(auto_now=False, auto_now_add=False)
+    
+
+    def __str__(self):
+        return (f"{self.firstname} {self.surname}")
+    
+class Appointment(models.Model):
+    APPOINTMENT_DAYS = (
+        ('Monday','Monday'),
+        ('Tuesday','Monday'),
+        ('Wednesday','Wednesday'),
+        ('Thursday','Thursday'),
+        ('Friday','Friday'),
+        ('Saturday','Saturday'),
+        ('Sunday','Sunday')
+    )
+    patient = models.ForeignKey(Patient, related_name ='patient_appointment' ,on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, related_name ='doctor_appointment', on_delete=models.CASCADE)
+    appointment_time = models.TimeField(auto_now=False, auto_now_add=False)
+    appointment_day = models.CharField(max_length=10, choices=APPOINTMENT_DAYS)
+    
+    def __str__(self):
+        return (f"{self.patient.firstname} {self.patient.surname}")
+
+   
+
